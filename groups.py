7299e06f3c90680a4153be2604b0853cecdd204b
algorythmist <dimitri@tecacet.com>
2022-06-13 03:35:32 +0000

    groups

---
+++ groups.py
@@ -0,0 +1,29 @@
+from integers import gcd
+
+
+def get_multiplicative_elements(n: int) -> list:
+    """
+    Compute all elements of the multiplicative group U(n)
+    :param n:
+    :return:
+    """
+    return [k for k in range(1, n) if gcd(k, n) == 1]
+
+
+def compute_cycle(a, n):
+    """
+    Compute <a> in U(n)
+    :param a:
+    :param n:
+    :return:
+    """
+    if a > n:
+        a = a % n
+    elems = [a]
+    product = a
+    for i in range(2, n):
+        if product == 1:
+            return elems
+        product = a*product % n
+        elems.append(product)
+    return elems
---
+++ tests/test_groups.py
@@ -0,0 +1,15 @@
+from groups import *
+
+
+def test_get_multiplicative_elements():
+    elems = get_multiplicative_elements(21)
+    assert [1, 2, 4, 5, 8, 10, 11, 13, 16, 17, 19, 20] == elems
+    elems = get_multiplicative_elements(36)
+    assert [1, 5, 7, 11, 13, 17, 19, 23, 25, 29, 31, 35] == elems
+
+
+def test_compute_cycle():
+    elems = compute_cycle(7, 8)
+    assert [7, 1] == elems
+    elems = compute_cycle(3, 10)
+    assert [3, 9, 7, 1] == elems
