321ddd57fdec2b0b71259743dd2209c221439358
algorythmist <dimitri@tecacet.com>
2022-06-13 01:42:05 +0000

    add CI

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='cryptalgebra',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/algorythmist/cryptalgebra',
+    author='Dimitri Papaioannou',
+    description='Algebraic cryptography',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9'
+)
